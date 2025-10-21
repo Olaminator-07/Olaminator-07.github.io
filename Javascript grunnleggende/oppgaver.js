6782867553425a32e672cb8621ce58a3223c6b6b
Olaminator-07 <superdupersoppen@gmail.com>
2025-10-21 15:48:37 +0200

    21.10

--- Javascript grunnleggende/oppgaver.js
+++ Javascript grunnleggende/oppgaver.js
@@ -35,15 +35,40 @@
 // let heltall = parseInt(binary, 2);
 // console.log(heltall);
 
-let verdi = 1;
-const knapp = document.getElementById("knapp");
-knapp.addEventListener("click", verdiendring);
 
-function verdiendring() {
-    verdi += 1;
-    console.log(verdi);
-    document.getElementById("score").innerText = ("Din score er " + verdi);
+// Kjeksklikker
+// let verdi = 1;
+// const knapp = document.getElementById("knapp");
+// knapp.addEventListener("click", verdiendring);
 
+// function verdiendring() {
+//     verdi += 1;
+//     console.log(verdi);
+//     document.getElementById("score").innerText = ("Din score er " + verdi);
+
+// }
+
+
+//Stedsnavn oppgave
+// let stedsnavn1 = prompt("Hva er det første stedsnavnet?");
+// let stedsnavn2 = prompt("Hva er det andre stedsnavnet?");
+// let lengde1 = stedsnavn1.length;
+// let lengde2 = stedsnavn2.length;
+// let differanse = Math.abs(lengde1 - lengde2);
+
+// console.log(lengde1);
+// console.log(lengde2);
+
+// document.getElementById("navn").innerText = ("Det første stedsnavnet er " + stedsnavn1 + ". Det andre stedsnavnet er " + stedsnavn2);
+// document.getElementById("lengde").innerText = ("Lengden på det første navnet er " + lengde1 + ". Lengden på det andre navnet er "+ lengde2);
+// document.getElementById("diff").innerText = ("Differansen i lengden av stedsnavnene er " + differanse);
+
+let i = 1;
+while (i <= 50){
+    console.log(i);
+    i++
 }
 
-
+for(let k=1; k<=50; k++){
+    console.log(k)
+}
