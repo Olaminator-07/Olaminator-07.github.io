e31907869e6433227386b15603589e736c01b787
Olaminator-07 <superdupersoppen@gmail.com>
2025-11-11 20:47:38 +0100

    jobbet videre med JS 11.11.25

--- Prosjekt før jul/script.js
+++ Prosjekt før jul/script.js
@@ -3,9 +3,9 @@
 
 const quizdata =[
     {
-        spørsmål: "Skriv spørsmålet her",
-        alternativer: ["alternativ", "alternativ2", "alternativ3", "alternativ4"],
-        svar: "alternativ" //det riktige alternativet
+        spørsmål: "Hvilket språk er best egnet for nettside utvikling",
+        alternativer: ["Scratch", "HTML", "Python", "Teams"],
+        svar: "HTML" //det riktige alternativet
     },
     {
         spørsmål: "Skriv spørsmål 2 her",
@@ -14,13 +14,39 @@ const quizdata =[
     }
 ];
 
-let spørsmålnummer = 0;
+let spørsmålnummer = -1;
 let score = 0;
 const spørsmålEL = document.querySelector(".spørsmål");
 const alternativerEL = document.querySelector(".alternativer");
-const gjeldendequiz = quizdata[spørsmålnummer];
+const scoreEl = document.getElementById("score");
+
+
+document.querySelector("#knappvidere").addEventListener("click", videre);
+// document.querySelector("#knapptilbake").addEventListener("click", tilbake);
+document.querySelector("#restart").addEventListener("click", restart);
+
+function videre(){
+    if (spørsmålnummer>= (quizdata.length - 1)) {
+        stoppquiz()
+    } else{
+    spørsmålnummer++;
+    lastinnspørsmål();
+    }
+}
+
+// function tilbake(){
+//     spørsmålnummer--;
+//     lastinnspørsmål();
+// }
+
+
+function stoppquiz(){
+    document.getElementById("slutt-tekst").innerText = "Quizen er ferdig! Trykk på 'Start på nytt' for å ta quizen igjen!"
+}
+
 
 function lastinnspørsmål(){
+    let gjeldendequiz = quizdata[spørsmålnummer];
     spørsmålEL.textContent = gjeldendequiz.spørsmål;
     alternativerEL.innerHTML = ''; 
     gjeldendequiz.alternativer.forEach(alternativ => {
@@ -30,39 +56,31 @@ function lastinnspørsmål(){
         knapp.onclick = () => checkAnswer(alternativ);
         alternativerEL.appendChild(knapp);
     });
-
 }
 
-function checkAnswer(alternativ) {
-            if (alternativ === quizdata[gjeldendequiz].svar) {
+function checkAnswer(valgtalternativ) {
+            if (valgtalternativ === quizdata[spørsmålnummer].svar) {
                 score++;
+                scoreEl.innerText = score;
             }
-            gjeldendequiz++;
-            lastinnspørsmål();
-        }
-
-
-
-document.querySelector("#knappvidere").addEventListener("click", videre);
-document.querySelector("#knapptilbake").addEventListener("click", tilbake);
-
-function videre(){
-    if (spørsmålnummer>= quizdata.length) {
-        stoppquiz()
-    } else{
-    spørsmålnummer++;
-    lastinnspørsmål();
-    }
-
-
-}
-
-function tilbake(){
-    spørsmålnummer--;
-    lastinnspørsmål();
+            spørsmålnummer++;
+        
+            if (spørsmålnummer < quizdata.length) {
+                lastinnspørsmål(); 
+             } else {
+            console.log("Quiz ferdig! Din score er: " + score);
+            stoppquiz()
+            }
 }
 
 
-function stoppquiz(){
+function restart() {
+    document.getElementById("slutt-tekst").innerText = '';
+    alternativerEL.innerHTML = '';
+    spørsmålEL.innerHTML = '';
     spørsmålnummer = 0;
+    score = 0;
+    scoreEl.innerText = score;
+    lastinnspørsmål();
 }
+
