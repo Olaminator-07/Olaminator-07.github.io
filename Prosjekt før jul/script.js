bede2465fe98929485bc4fe2fb11c1bc48181bfd
Olaminator-07 <superdupersoppen@gmail.com>
2025-11-21 10:25:51 +0100

    21.11
    Arbeid i IT timene

--- Prosjekt før jul/script.js
+++ Prosjekt før jul/script.js
@@ -36,6 +36,11 @@ const quizdata =[
         spørsmål: "Hvilken av følgende er en gyldig JavaScript-variabeldeklarasjon?",
         alternativer: ["var 1number = 10;", "let number = 10;", "int number = 10;", "value number = 10;"],
         svar: "let number = 10;" 
+    },
+    {
+        spørsmål: "Hvilket av følgende beskriver best hva HTML brukes til i nettsideutvikling?",
+        alternativer: ["Å styre utseendet og layouten på nettsiden", "Å strukturere innholdet på nettsiden", "Å håndtere serverlogikk og databaser", "Å optimalisere nettsidens ytelse og lastetid"],
+        svar: "Å strukturere innholdet på nettsiden"
     }
 
 ];
@@ -45,7 +50,6 @@ let quiz = quizdata.slice().sort(function(){return Math.random() - 0.5;});
 
 let spørsmålnummer = 0; 
 let score = 0;
-let progression = 0;
 const spørsmålEL = document.querySelector(".spørsmål");
 const alternativerEL = document.querySelector(".alternativer");
 const scoreEl = document.getElementById("score");
@@ -56,6 +60,7 @@ document.querySelector("#restart").addEventListener("click", restart);
 function lastinnspørsmål(){
     let gjeldendequiz = quiz[spørsmålnummer];
     spørsmålEL.innerText = gjeldendequiz.spørsmål;
+    document.getElementById("steg" + (spørsmålnummer+1)).style.backgroundColor = "rgba(4, 168, 15, 1)";  //Oppdaterer fargen i progressionbar
     alternativerEL.innerText = ''; 
     gjeldendequiz.alternativer.forEach(function(alternativ) {
         const knapp = document.createElement('button');
@@ -74,9 +79,7 @@ function checkAnswer(valgtalternativ) {
                 scoreEl.innerText = score;
             }
             spørsmålnummer++;
-            progression ++;
-            console.log(progression);
-
+            
             if (spørsmålnummer < quizdata.length) {
                 lastinnspørsmål(); 
              } else {
@@ -93,6 +96,10 @@ function restart() {
     spørsmålnummer = 0;
     score = 0;
     scoreEl.innerText = score;
+    const steg_div = document.querySelectorAll("#progression-bar > div"); //Oppdaterer fargen i progressbar tilbake til grå
+    steg_div.forEach(function(steg){
+        steg.style.backgroundColor = "grey";
+    });
     quiz = quizdata.slice().sort(function(){return Math.random() -0.5;});
     lastinnspørsmål();
 }
@@ -100,7 +107,6 @@ function restart() {
 function stoppquiz(){
     document.getElementById("slutt-tekst").innerText = "Quizen er ferdig! Trykk på 'Start på nytt' for å ta quizen igjen!"
 }
-
 
 
 
