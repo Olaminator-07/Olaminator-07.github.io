932cc20a1e02e531cf4dd4ac42d5fdd72bc0c864
Olaminator-07 <superdupersoppen@gmail.com>
2026-02-23 10:11:59 +0100

    Arbeid i forbindelse med proven 13.02

--- prove_13.02.26/Oppgave 2/script.js
+++ prove_13.02.26/Oppgave 2/script.js
@@ -1,49 +1,51 @@
+let data;
+
 async function hentData() { 
-    const res = await fetch('https://www.anapioficeandfire.com/api/books/'); //Fetcher apien
-    const data = await res.json();                                           //Lagrer info fra api som JSON fil
+    let res = await fetch('https://www.anapioficeandfire.com/api/books/'); //Fetcher apien
+    data = await res.json();                                           //Lagrer info fra api som JSON fil
     
-    document.querySelector("#names").innerHTML = "Navn på bøkene: ";               //Tømmer div boksene names, utgivelse og sidetall i HTML
-    document.querySelector("#utgivelse").innerHTML = "Utgivelsesdatoer: ";
+    document.querySelector("#names").innerHTML = "";               //Tømmer div boksene names, utgivelse og sidetall i HTML
+    document.querySelector("#utgivelse").innerHTML = "Utgivelsesdato: ";
     document.querySelector("#sidetall").innerHTML = "Antall sidetall: ";
 
     for (let i = 0; i < data.length; i++) {
-        let navn = document.createElement("p");                //for hvert element i arrayen data, så skrives navnet, utgivelse og sidetall henholdsvis i hver sin div boks
+        let navn = document.createElement("button");                //for hvert element i arrayen data, så skrives navnet, utgivelse og sidetall henholdsvis i hver sin div boks
         navn.innerText = data[i].name;
         document.querySelector("#names").appendChild(navn);
-
-         let released = document.createElement("p");
-        released.innerText = data[i].released;
-        document.querySelector("#utgivelse").appendChild(released);
-
-        let numberOfPages = document.createElement("p");
-        numberOfPages.innerText = data[i].numberOfPages;
-        document.querySelector("#sidetall").appendChild(numberOfPages); 
+        navn.id = i;
     }
+    lyttefunksjon();
+
 }
 hentData();
 
+function lyttefunksjon(){
+    for (let i = 0; i < data.length; i++) {
+        document.getElementById(i).addEventListener("click", function(){
+            visData(i);
+        });   
+    } 
+}
+       
+function visData(tall){
+    document.querySelector("#utgivelse").innerHTML = "Utgivelsesdato:  ";
+    document.querySelector("#sidetall").innerHTML = "Antall sidetall:  ";    
 
-//Jeg tenkte jeg kunne lage en knapp for hver av bøkene, med navn som tekst. Så kunne jeg legge en eventlistner på hver knapp
-//slik at når f.eks. knapp til elemnt 1 blir trykket, vises utgivelses år og sidetall til dette elementet i data. Problemet er at jeg ikke fikk laget en bestemt
-//id for hver enkelt knapp jeg kunne bruke. Se koden under sånn ca hva jeg tenkte
+    let released = document.createElement("p");
+    released.innerText = data[tall].released;
+    document.querySelector("#utgivelse").appendChild(released);
+
+    let numberOfPages = document.createElement("p");
+    numberOfPages.innerText = data[tall].numberOfPages;
+    document.querySelector("#sidetall").appendChild(numberOfPages); 
+};
 
 
-// "knappen til en bestemt tittel".addEventListener("click", function(){
-//     visData();
-//  });
 
 
-// function visData(){
-//     for (let i = 0; i < data.length; i++) {
-//         let released = document.createElement("p");
-//         released.innerText = data[i].released;
-//         document.querySelector("#utgivelse").appendChild(released);
 
-//         let numberOfPages = document.createElement("p");
-//         numberOfPages.innerText = data[i].numberOfPages;
-//         document.querySelector("#sidetall").appendChild(numberOfPages); 
-//     }
-// };
+
+
 
 
 
